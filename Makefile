879946fdf478d0d90c726930abe69de50c2d8544
ACLzz <aclz@protonmail.com>
2025-05-07 00:32:48 +0300

    fix protobuf package paths and add graceful shutdown for comm server

--- Makefile
+++ Makefile
@@ -1,11 +1,25 @@
 FLAGS = -gcflags=all=-l -parallel=1
+PROTO = protoc \
+		--proto_path=protobuf \
+		--go_opt=paths=source_relative
 
 dep:
 	go mod tidy
 
 proto:
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-	protoc --proto_path=protobuf --go_out=protobuf protobuf/*.proto --go_opt=paths=import
+
+	mkdir -p ./protobuf/gen/securegcm
+	$(PROTO) --go_out=protobuf/gen/securegcm ./protobuf/device_to_device_messages.proto ./protobuf/securegcm.proto ./protobuf/ukey.proto
+	
+	mkdir -p ./protobuf/gen/connections
+	$(PROTO) --go_out=protobuf/gen/connections ./protobuf/offline_wire_formats.proto
+
+	mkdir -p ./protobuf/gen/securemessage
+	$(PROTO) --go_out=protobuf/gen/securemessage ./protobuf/securemessage.proto
+	
+	mkdir -p ./protobuf/gen/sharing
+	$(PROTO) --go_out=protobuf/gen/sharing ./protobuf/wire_format.proto
 
 clean:
 	rm -rfv ./protobuf/gen
--- server/comm.go
+++ server/comm.go
@@ -1,14 +1,24 @@
 package server
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"net"
+	"sync"
+
+	"github.com/ACLzz/go-qshare/protobuf/gen/connections"
+	"google.golang.org/protobuf/proto"
 )
 
 type commServer struct {
-	sock net.Listener
+	sock   net.Listener
+	wg     *sync.WaitGroup
+	stopCh chan struct{}
 	// TODO: use kind of logger instead of prints
 }
+
+const batchSize = 256
 
 func newCommServer(port int) (commServer, error) {
 	sock, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
@@ -17,47 +27,69 @@ func newCommServer(port int) (commServer, error) {
 	}
 
 	return commServer{
-		sock: sock,
+		sock:   sock,
+		wg:     &sync.WaitGroup{},
+		stopCh: make(chan struct{}, 1),
 	}, nil
 }
 
 func (cs commServer) Listen() {
+	var (
+		conn net.Conn
+		err  error
+	)
+	cs.wg.Add(1)
+	defer cs.wg.Done()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	for {
-		conn, err := cs.sock.Accept()
+		conn, err = cs.sock.Accept()
 		if err != nil {
-			fmt.Printf("ERR: accept conn: %s", err.Error())
+			select {
+			case <-cs.stopCh:
+				return
+			default:
+				fmt.Printf("ERR: accept conn: %s", err.Error())
+			}
 			continue
 		}
-		go cs.handle(conn) // TODO: maybe limit amount of gorutines here
+
+		cs.wg.Add(1)
+		go cs.handleConn(ctx, conn) // TODO: maybe limit amount of gorutines here
 	}
 }
 
-func (cs commServer) handle(conn net.Conn) {
+func (cs commServer) handleConn(ctx context.Context, conn net.Conn) {
 	var (
-		n   int
-		err error
-		buf = make([]byte, 0)
+		n        int
+		err      error
+		buf      bytes.Buffer
+		batchBuf = make([]byte, batchSize)
 	)
+	defer cs.wg.Done()
 
 	for {
-		if n, err = conn.Read(buf); err != nil {
-			fmt.Printf("ERR: read conn: %s", err.Error())
-			continue
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if n, err = conn.Read(batchBuf); err != nil {
+				fmt.Printf("ERR: read conn: %s", err.Error())
+				continue
+			}
+
+			buf.Write(batchBuf)
+			if n < batchSize {
+				var frame connections.ConnectionRequestFrame
+				if err := proto.Unmarshal(buf.Bytes(), &frame); err != nil {
+					fmt.Printf("ERR: unmarshal: %s", err.Error())
+				}
+
+				fmt.Println(frame.String())
+				return
+			}
 		}
-
-		fmt.Printf("recv (%d): '%s'", len(buf), string(buf))
-
-		if n == 0 {
-			break
-		}
-
-		if _, err := conn.Write(buf); err != nil {
-			fmt.Printf("ERR: write conn: %s", err.Error())
-		}
-	}
-
-	if err = conn.Close(); err != nil {
-		fmt.Printf("ERR: close conn: %s", err.Error())
 	}
 }
 
@@ -66,10 +98,11 @@ func (cs commServer) Stop() error {
 		return nil
 	}
 
-	// TODO: gracefully close all conns and exit from Listen loop
+	cs.stopCh <- struct{}{}
 	if err := cs.sock.Close(); err != nil {
 		return fmt.Errorf("close socket: %w", err)
 	}
+	cs.wg.Wait()
 
 	return nil
 }
